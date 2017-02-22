150d03fb9f280ef3a87305b5830b19f3f2547dde
Clément Hennebelle <clement.hennebelle@hotmail.fr>
2017-02-22 15:57:59 -0500

    Implemented basic TPS camera

---
+++ Assets/Scripts/Camera_RTS.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_RTS : MonoBehaviour {
+	
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
