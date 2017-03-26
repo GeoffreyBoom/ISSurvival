86173696c6fd31a25124e9f0452c600a095b09ae
Audrey Jw <Audrey Jw>
2017-03-26 14:54:35 -0400

    Networking with Pun & automatic door

---
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+
+public class NetworkManager : Photon.PunBehaviour {
+
+    private string VERSION = "v0.0.1";
+    public string roomName = "ISS";
+
+    [SerializeField]
+    string playerPrefabName = "";
+
+
+
+    // Use this for initialization
+    void Start () {
+
+        PhotonNetwork.ConnectUsingSettings(VERSION);
+        PhotonNetwork.autoJoinLobby = true;
+
+    }
+
+    void OnGUI()
+    {
+
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+
+    public override void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinRandomRoom();
+        //create a room for multiplayer:
+      /*  RoomOptions roomOptions = new RoomOptions();
+        roomOptions.IsVisible = false;
+        roomOptions.MaxPlayers = 2; // 2 player per room 
+        PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, TypedLobby.Default);*/
+    }
+
+
+    void OnPhotonRandomJoinFailed()
+    {
+
+        Debug.Log("Can't join random room!  DON'T MIND ME, I'LL JUST MAKE MY OWN ROOM.");
+        PhotonNetwork.CreateRoom(null);
+    }
+
+
+    //Spawn player:
+    void OnJoinedRoom()
+    {
+
+        Debug.Log(DataHolder.player);
+         
+        if (DataHolder.player == false)
+        {
+            Debug.Log("I am here");
+        //when we want to instantiate objects that we want ALL player to see:
+        PhotonNetwork.Instantiate(playerPrefabName, this.transform.position, this.transform.rotation, 0);
+        }
+    
+    }
+}
---
+++ Assets/Scripts/UI Menu Script/CameraControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class CameraControl : MonoBehaviour {
+
+    //Change camera position based on empty game objects placed in the world. 
+    public Transform camPosAndRot;
+    [SerializeField]
+    float smoothSpeedCam = 0.1f;
+
+    AsyncOperation loadLevelCompletely;
+
+
+    // Update is called once per frame
+    void Update ()
+    {
+        transform.position = Vector3.Lerp(transform.position,camPosAndRot.position, smoothSpeedCam);
+        transform.rotation = Quaternion.Slerp(transform.rotation, camPosAndRot.rotation, smoothSpeedCam);    
+    }
+
+    public void setNextSpace(Transform nextSpace)
+    {     
+        camPosAndRot = nextSpace;
+    }
+
+    //Make sure that the scene is fully loaded before doing anything else: 
+    IEnumerator switchScene()
+    {
+        loadLevelCompletely = SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);
+        while (!loadLevelCompletely.isDone) yield return null;
+    }
+
+
+    /* This function is called when pressing the the "GOD" or "Survivor" player selectors. It loads the game testCamera scene and sets 
+     the DataHolder.player static variable which will enable the right camera for the selected player: */
+    public void selectPlayer()
+    {
+        GameObject clickedButton = EventSystem.current.currentSelectedGameObject;
+
+        if (clickedButton.name == GameObject.Find("TPS Player Button").name)
+        {
+            DataHolder.player = false; 
+
+        }
+        else if (clickedButton.name == GameObject.Find("RTS Player Button").name)
+        {
+            DataHolder.player = true;
+        }
+
+        StartCoroutine(switchScene());
+    }
+}
