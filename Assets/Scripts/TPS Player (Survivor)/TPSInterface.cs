8fcab77bc6e21c19434ce14f3dd9d9639f2681c3
geoffreyboom <geoff.the.boom@gmail.com>
2017-04-12 19:01:59 -0400

    applied the update properly

---
+++ Assets/Scripts/TPS Player (Survivor)/TPSInterface.cs
@@ -0,0 +1,52 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class TPSInterface : MonoBehaviour {
+    public Slider healthBar;
+    public Slider staminaBar;
+  //  public ThirdPersonCharacter player;
+
+    public int currentHealth;
+    int currentStamina, amount = 2;
+    float nextMove, moveRate = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+        currentHealth = 100;
+        currentStamina = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (TPSPlayer.setDamageUI == true)
+        {
+            setHealthUI();
+        }
+
+        if(Input.GetKey(KeyCode.LeftShift) && staminaBar.value > 0 && Time.time > nextMove)
+        {
+            nextMove = Time.time + moveRate;
+            setStaminaUI();
+        }
+
+    }
+
+    private void setHealthUI()
+    {
+        currentHealth -= amount;
+
+        healthBar.value = currentHealth;
+    }
+
+    private void setStaminaUI()
+    {
+        currentStamina -= amount;
+
+        staminaBar.value = currentStamina;
+    }
+}
