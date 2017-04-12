72c73c8cb8929a1b325230fdd9ce1b83e58c0573
geoffreyboom <geoff.the.boom@gmail.com>
2017-04-12 04:29:24 -0400

    added bullets

---
+++ Assets/Bullet.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	float StartTimestamp;
+
+	[SerializeField]
+	float LifeTime = 2000;
+	[SerializeField]
+	Rigidbody rigid;
+
+	// Use this for initialization
+	void Start () {
+		StartTimestamp = Time.time;
+		
+	}
+
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Time.time - StartTimestamp < LifeTime)
+		{
+			Destroy(this.gameObject);
+		}
+	}
+
+	void SetInitialAcceleration(float velocity, Vector3 position, Vector3 direction)
+	{
+		transform.position = position;
+		transform.forward = direction;
+		if (rigid == null)
+		{
+			rigid = gameObject.GetComponent<Rigidbody>();
+		}
+		if (rigid != null)
+		{
+			rigid.velocity = transform.forward.normalized * velocity;
+		}
+		else
+		{
+			print("bullet does not have RigidBody");
+		}
+	}
+	void OnTriggerEnter(Collider other)
+	{
+
+		//TODO: implement the behavior for networking.
+		TPSPlayer OtherPlayer = null;
+		if ((OtherPlayer = other.gameObject.GetComponent<TPSPlayer>() )!= null)
+		{
+			OtherPlayer.TakeDamage();
+		}
+
+		//TODO: implement enemy health reduction
+	}
+}
--- Assets/Scripts/TPSPlayer.cs
+++ Assets/Scripts/TPSPlayer.cs
@@ -18,7 +18,7 @@ public class TPSPlayer : MonoBehaviour {
 		
 	}
 
-    void TakeDamage()
+    public void TakeDamage()
     {
 
     }
